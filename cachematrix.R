0a6729de8b420c2315e74cbbcb5443fd7ebf45ca
Islam Al-Aarag <islam.alaarag@gmail.com>
2015-02-22 14:24:20 -0800

    Adding functions logic to cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special object that
+## is capable of getting/setting a matrix inverse
+## without recomputing it as long as the data
+## is the same.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	cachedInv <- NULL
+	set <- function(y) {
+		x <<- y
+		cachedInv <<- NULL
+	}
+	get <- function() x
+	setInv <- function(inv) cachedInv <<- inv
+	getInv <- function() cachedInv
+	list(set = set, get = get,
+		 setInv = setInv,
+		 getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of a matrix
+## if the data is new or gets it from the cache if
+## computed already.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+	if (!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setInv(inv)
+	inv
 }
